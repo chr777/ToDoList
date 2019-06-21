4b8e86e529dae2647946ec8933be2d6bcf38cacd
christine77777 <christine_gevorgyan@aybschool.am>
2019-06-21 16:05:54 +0400

    adding todolist.js and changing the app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, message: 'do homeworks'},
+      {id: 2, message: 'play games'}
+
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    });
+  }
+
+  render() {
+    return (
+      <div className="todolist list_container">
+        <h1 className="center text">Todo's</h1>
+        <Todos todos={this.state.todos}  />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
